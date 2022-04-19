f81233021335eff527d6b44cb33a0bcffc850566
Adrian Jung <adrian.mael.jung@gmail.com>
2022-04-19 20:35:01 +0200

    Start project, lets goo

---
+++ philo/inc/philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 19:24:51 by ajung             #+#    #+#             */
+/*   Updated: 2022/04/19 20:23:52 by ajung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+// struct timeval {
+//     time_t      tv_sec;  /* secondes */
+//     suseconds_t tv_usec; /* microsecondes */
+// } t_timeval;
+
+typedef	struct timeval t_timeval;
+
+
+#endif
