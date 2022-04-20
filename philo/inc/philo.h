95414d86dc17e2da4f3ff66841143077fbb0f9bc
Adrian JUNG <ajung@e1r11p19.42.fr>
2022-04-20 17:58:37 +0200

    lecture man

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 19:24:51 by ajung             #+#    #+#             */
-/*   Updated: 2022/04/19 20:23:52 by ajung            ###   ########.fr       */
+/*   Updated: 2022/04/20 17:55:52 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,28 @@
 # include <string.h>
 # include <unistd.h>
 # include <sys/time.h>
+# include <pthread.h>
 
-// struct timeval {
-//     time_t      tv_sec;  /* secondes */
-//     suseconds_t tv_usec; /* microsecondes */
-// } t_timeval;
+
 
 typedef	struct timeval t_timeval;
 
+# define SUCCESS 0
+# define FAILURE (!SUCCESS)
 
+/* 
+struct timeval {
+    time_t      tv_sec;  --> secondes
+    suseconds_t tv_usec; --> microsecondes
+}; 
+
+
+int gettimeofday(struct timeval *tv, NULL);
+
+int pthread_create(pthread_t *restrict thread,
+                          const pthread_attr_t *restrict attr,
+                          void *(*start_routine)(void *),
+                          void *restrict arg);
+
+*/
 #endif
